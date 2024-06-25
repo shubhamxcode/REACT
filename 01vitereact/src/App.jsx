e5a7668efba485d87b997b7449beb64a9c96f906
shubhamxcode <shubh.varshneycode@gmail.com>
2024-06-25 14:07:24 +0530

    variable

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,12 +2,12 @@ import Chai from "./chai"
 
 function App() {
 
+  const variable="React is very powerfull"//called expression
   return (
     <>
     <Chai/>
-    <h1>3 cutting chai</h1>
+    <h1>3 cutting chai {variable} </h1> 
     </>
   )
 }
-
 export default App
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+
+function Myapp(){
+    return(
+    <div>
+        <h1>shubham varshney is learning react</h1>
+    </div>
+    )
+}
+
+const reactelement={
+    type:'a',
+    props:{
+        href:'https://google.com',
+        target:'_blank'
+    },
+    children:'click me to visit google'
+}
+
+const anotherelement=(
+    <a href="https://google.com">go to google</a>
+)
+
+const username="react is all about j.s"
+const Reacts=React.createElement(
+    'a',//tag
+    {href:'https://youtube.com', target:'_blank'},
+    'go to youtube shubham sir',
+    username   
+)
 ReactDOM.createRoot(document.getElementById('root')).
 render(
-    <App />
+    Reacts
 )
