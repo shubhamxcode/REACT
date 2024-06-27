cf3a37d554f14f1c1071a64f062356c79feb2e6c
shubhamxcode <shubh.varshneycode@gmail.com>
2024-06-27 11:53:51 +0530

    hooks

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -3,21 +3,38 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
 function App() {
-let[counter,setcounter]=useState(1)
-function value(){
-  setcounter(counter+1)
-}
-function remove(){
-  setcounter(counter-1)
-}
+
+  const [counter, setCounter]  = useState(15)
+
+  //let counter = 15
+
+  const addValue = () => {
+    //counter = counter + 1
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1 )
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1)
+  }
+  
   return (
     <>
-    <h1>shubham varshney {counter}</h1>
-    <h2>counter value {counter}</h2>
-    <button onClick={value}>Add value</button> <br />
-    <button onClick={remove}>remove value</button>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add value {counter}</button> 
+      <br />
+      <button
+      onClick={removeValue}
+      >remove value {counter}</button>
+      <p>footer: {counter}</p>
     </>
   )
 }
